06c9bad80565ccc613b7acc0c08eabb3ca078739
one1lizeron <one1lizeron@gmail.com>
2023-11-06 21:15:08 +0800

    init spring-boot

---
+++ src/test/java/com/one1/mall/MallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one1.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
